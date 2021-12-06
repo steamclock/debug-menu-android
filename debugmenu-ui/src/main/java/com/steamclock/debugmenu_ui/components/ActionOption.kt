f59fe66e880f5c65159d1ed4c126ce5f2520e77f
Jacob Miner <jake@steamclock.com>
2021-12-06 14:58:55 -0800

    Make action onClick suspending

--- debugmenu-ui/src/main/java/com/steamclock/debugmenu_ui/components/ActionOption.kt
+++ debugmenu-ui/src/main/java/com/steamclock/debugmenu_ui/components/ActionOption.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.steamclock.debugmenu.Action
+import kotlinx.coroutines.launch
 
 /**
  * debugmenu
@@ -13,8 +15,9 @@ import com.steamclock.debugmenu.Action
  */
 @Composable
 fun ActionOption(option: Action) {
+    val scope = rememberCoroutineScope()
     Button(modifier = Modifier.fillMaxWidth(),
-        onClick = option.onClick) {
+        onClick = { scope.launch { option.onClick() } }) {
         Text(option.title)
     }
 }
