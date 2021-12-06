1664003fee383fde1c8f9187e496f910821d3619
Jacob Miner <jake@steamclock.com>
2021-12-06 14:47:49 -0800

    Initialize DebugMenu in App

---
+++ app/src/main/java/com/steamclock/debugmenusample/App.kt
@@ -0,0 +1,22 @@
+package com.steamclock.debugmenusample
+
+import android.app.Application
+import com.steamclock.debugmenu.DebugMenu
+import com.steamclock.debugmenu_sharedprefs.SharedPrefsPersistence
+import com.steamclock.debugmenu_ui.ComposeDebugMenuDisplay
+
+/**
+ * debugmenu
+ * Created by jake on 2021-12-06, 10:24 a.m.
+ */
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DebugMenu.initialize("123321",
+            ComposeDebugMenuDisplay().apply {
+                attach(this@App)
+            },
+            SharedPrefsPersistence(this)
+        )
+    }
+}
