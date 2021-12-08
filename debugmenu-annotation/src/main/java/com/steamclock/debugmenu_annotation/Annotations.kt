ecd71a04f192f6b06e06267bf84b17bb58f111f8
Jacob Miner <jake@steamclock.com>
2021-12-08 14:29:27 -0800

    Create Annotations.kt

---
+++ debugmenu-annotation/src/main/java/com/steamclock/debugmenu_annotation/Annotations.kt
@@ -0,0 +1,20 @@
+package com.steamclock.debugmenu_annotation
+
+/**
+ * debugmenu
+ * Created by jake on 2021-12-08, 2:11 p.m.
+ */
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class DebugMenuApp
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class DebugToggle(val title: String, val defaultValue: Boolean = false)
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class DebugAction(val title: String)
