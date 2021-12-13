0a17fe0433c4e912fb31ba7b2e1a9ed2fdb87e3e
Jacob Miner <jake@steamclock.com>
2021-12-13 13:21:19 -0800

    Add display for string selection

--- debugmenu-ui/src/main/java/com/steamclock/debugmenu_ui/components/Menu.kt
+++ debugmenu-ui/src/main/java/com/steamclock/debugmenu_ui/components/Menu.kt
@@ -31,6 +31,7 @@ fun Menu(title: String, options: List<DebugOption>) {
                     is DoubleValue -> DoubleOption(option)
                     is IntValue -> IntOption(option)
                     is LongValue -> LongOption(option)
+                    is StringSelection -> StringSelection(option)
                 }
             }
         }
---
+++ debugmenu-ui/src/main/java/com/steamclock/debugmenu_ui/components/StringSelection.kt
@@ -0,0 +1,52 @@
+package com.steamclock.debugmenu_ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import com.steamclock.debugmenu.DebugMenu
+import com.steamclock.debugmenu.StringSelection
+import com.steamclock.debugmenu.flow
+import com.steamclock.debugmenu.update
+import kotlinx.coroutines.launch
+
+/**
+ * debugmenu
+ * Created by jake on 2021-12-13, 1:16 p.m.
+ */
+@Composable
+fun StringSelection(option: StringSelection) {
+    val composableScope = rememberCoroutineScope()
+    val expanded = remember { mutableStateOf(false) }
+    val selectedIndex = DebugMenu.instance.flow<Int>(option.key).collectAsState(initial = option.defaultIndex)
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(
+            option.options[selectedIndex.value],
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(onClick = { expanded.value = true })
+        )
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            option.options.forEachIndexed { index, string ->
+                DropdownMenuItem(onClick = {
+                    expanded.value = false
+                    composableScope.launch {
+                        DebugMenu.instance.update(option.key, index)
+                    }
+                }) {
+                    Text(string)
+                }
+            }
+        }
+    }
+}
