a9357b006066756efb01134b8a6eab7d61875fbb
Jacob Miner <jake@steamclock.com>
2021-12-13 14:59:00 -0800

    Move key into parent
    Also make OptionSelection use String instead of Any

--- debugmenu/src/main/java/com/steamclock/debugmenu/DebugOption.kt
+++ debugmenu/src/main/java/com/steamclock/debugmenu/DebugOption.kt
@@ -4,10 +4,10 @@ package com.steamclock.debugmenu
  * debugmenu
  * Created by jake on 2021-12-03, 2:33 p.m.
  */
-sealed class DebugOption(open val title: String)
-data class BooleanValue(override val title: String, val key: String, val defaultValue: Boolean = false): DebugOption(title)
-data class IntValue(override val title: String, val key: String, val defaultValue: Int = 0): DebugOption(title)
-data class DoubleValue(override val title: String, val key: String, val defaultValue: Double = 0.0): DebugOption(title)
-data class LongValue(override val title: String, val key: String, val defaultValue: Long = 0L): DebugOption(title)
-data class Action(override val title: String, val onClick: suspend () -> Unit): DebugOption(title)
-data class OptionSelection(override val title: String, val key: String, val options: List<Any>, val defaultIndex: Int? = null): DebugOption(title)
+sealed class DebugOption(open val title: String, open val key: String)
+data class BooleanValue(override val title: String, override val key: String, val defaultValue: Boolean = false): DebugOption(title, key)
+data class IntValue(override val title: String, override val key: String, val defaultValue: Int = 0): DebugOption(title, key)
+data class DoubleValue(override val title: String, override val key: String, val defaultValue: Double = 0.0): DebugOption(title, key)
+data class LongValue(override val title: String, override val key: String, val defaultValue: Long = 0L): DebugOption(title, key)
+data class Action(override val title: String, val onClick: suspend () -> Unit): DebugOption(title, title)
+data class OptionSelection(override val title: String, override val key: String, val options: List<String>, val defaultIndex: Int? = null): DebugOption(title, key)
