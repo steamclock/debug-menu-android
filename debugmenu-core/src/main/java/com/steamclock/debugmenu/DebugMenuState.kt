6c1882faaeb1a2f6bac8ca81f0ca57b6915aa650
Jacob Miner <jake@steamclock.com>
2022-03-23 13:54:24 -0700

    Add header and footer to DebugMenuState

--- debugmenu-core/src/main/java/com/steamclock/debugmenu/DebugMenuState.kt
+++ debugmenu-core/src/main/java/com/steamclock/debugmenu/DebugMenuState.kt
@@ -6,6 +6,8 @@ import com.steamclock.debugmenu.persistence.DebugMenuPersistence
 import com.steamclock.debugmenu.persistence.InMemoryPersistence
 
 data class DebugMenuState(val title: String,
+                          val header: String? = null,
+                          val footer: String? = null,
                           val options: Map<String, List<DebugOption>> = mapOf(),
                           val persistence: DebugMenuPersistence = InMemoryPersistence(),
                           val display: DebugMenuDisplay = LogDisplay())
